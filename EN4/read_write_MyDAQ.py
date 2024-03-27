1dddb08e6faf2bf5d8dc752fa41176c659d4bcd8
Sam Lamboo <125293490+NerdyN00B@users.noreply.github.com>
2024-03-27 17:10:28 +0100

    Update read_write_MyDAQ.py

--- EN4/read_write_MyDAQ.py
+++ EN4/read_write_MyDAQ.py
@@ -1,44 +1,64 @@
-## Code met dank aan Meinhard
+"""
+Code voor uitlezen MyDAQ met dank aan Meinhard
 
+Aangepast voor metingen door Sam Lamboo
+"""
 import Mydaqclass as My
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from scipy import integrate
 
-
+savepath = 'P:\EN4\Daadwerkelijke_metingen\\'
 savefile = input('hoe wil je de datafile noemen? (zonder extension)\n')
-savefile += '.csv'
-
 
 def sweep(time, vpp=7, start=300, stop=15000):
-    amplitude = vpp/2
+    amplitude = vpp / 2
     frequency_difference = stop - start
-    linear_frequency = 300 + (frequency_difference / time[-1]) * time
-    offset = 0 + amplitude
+    linear_frequency = start + (frequency_difference / time[-1]) * time
+    offset = 0 
     return offset + amplitude * np.sin(2 * np.pi * linear_frequency * time)
 
 
 
 mydaq = My.MyDAQ()
 
-t = 1 #s
+t = 2 #s
 rate = 200000 #Hz
 N = rate*t #total samples
 
 time = np.linspace(0, N/rate, N)
 
-audio_sweep = sweep(time)
+audio_sweep = sweep(time, start=300, stop=15000)
+
+audio_sweep[-1] = 0
+
+voltagearray1 = np.linspace(0,1,N)
+
+
+iters = 5
+
+fig, ax = plt.subplots(layout='tight')
+
+ax.set_xscale('log')
+ax.axis(ymin=None, ymax=2500, xmin=200, xmax=16000)
+
+for i in range(iters):
+    print(f"measurement {i}")
+    data = mydaq.writeread(rate, N, audio_sweep)
     
-voltagearray1 = np.zeros(len(time))
+    np.savetxt(savepath + savefile + f'_{i}'+'.csv', np.c_[data, time],
+               delimiter=',')
+    
+    fourier = np.fft.fft(data)
+    frequencies = np.fft.fftfreq(time.size, time[1]-time[0])
+    
+    ax.plot(frequencies, np.abs(fourier), label=savefile + f'_{i}')
 
 
-data = mydaq.writeread(rate, N, list(audio_sweep))
-
-np.savetxt(savefile, data, delimiter=',')
-
-plt.plot(time,data)
-plt.xlabel("Time (s)")
-plt.ylabel("Voltage")
-plt.title("Plot by dividing signals analog with divider circuit")
-plt.show()
+ax.set_xlabel("Frequency [Hz]")
+ax.set_ylabel("Intensity")
+ax.set_title(savefile)
+ax.legend()
+fig.savefig(savepath + savefile + '.png')
+fig.show()
